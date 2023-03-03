a4d5dc53e542db24a2bc0e2425d2a67e407bb8c4
SuCongLin <1799557611@qq.com>
2023-03-03 22:43:51 +0800

    java练习

---
+++ src/JavaCode/Hero.java
@@ -0,0 +1,46 @@
+package JavaCode;
+
+public class Hero{
+    String Name;//姓名
+    float Hp;//血量
+    float Armor;//护甲
+    int MoveSpeed;//移速
+    int kill;//人头
+    int killed;//送头
+    int GoldCoin;//金币
+    void Keng(){//坑队友加金币
+        System.out.println("谢谢你，泰罗");
+        GoldCoin+=50;
+    }
+    float GetArmor(){//返回护甲值
+        return Armor;
+    }
+    void addSpeed(int Speed){//增加移速
+        MoveSpeed+=Speed;
+    }
+    void legendary(){//超神提示
+        System.out.println("超神！");
+    }
+    float GetHp(){//返回血量
+        return Hp;
+    }
+    void recovery(float blood){//加血
+        Hp+=blood;
+    }
+
+
+    public static void main(String[] args){
+        Hero GaRen = new Hero();
+        GaRen.Hp=10000f;
+        GaRen.Name="盖伦";
+        GaRen.Armor=1200f;
+        GaRen.MoveSpeed=345;
+        GaRen.addSpeed(50);
+
+        Hero TeeMo=new Hero();
+        TeeMo.Hp=4800f;
+        TeeMo.Name="提莫";
+        TeeMo.Armor=800f;
+        TeeMo.MoveSpeed=345;
+    }
+}
---
+++ src/JavaCode/item.java
@@ -0,0 +1,18 @@
+package JavaCode;
+public class item{
+    String Name;
+    int Price;
+    public static void main(String[] args){
+        item BloodBottle =new item();
+        BloodBottle.Name="血瓶";
+        BloodBottle.Price=50;
+
+        item Shoes=new item();
+        Shoes.Name="草鞋";
+        Shoes.Price=300;
+
+        item Sword =new item();
+        Sword.Name="大宝剑";
+        Sword.Price=300;
+    }
+}
